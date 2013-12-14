8ac99c0e5d84deb67291085646d80822331eed37
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2013-12-14 12:08:43 -0600

    added UI for adding values for dropdown fields

--- liber-web/src/main/webapp/viewmodels/field_create.js
+++ liber-web/src/main/webapp/viewmodels/field_create.js
@@ -1,13 +1,22 @@
-define( [], function() {
+define( ['knockout'], function( ko ) {
 	var model = function() {
 		var self = this;
 		
 		self.name = ko.observable();
 		self.type = ko.observable();
+		self.values = ko.observableArray( [] );
+		
+		self.addValue = function() {
+			self.values.push( { value: '' } );
+		};
+		self.removeValue = function( value ) {
+			self.values.remove( value );
+		};
 		
 		self.createField = function() {
 			alert( self.name() );
-		}
+		};
+		
 	};
 	return new model();
 })
