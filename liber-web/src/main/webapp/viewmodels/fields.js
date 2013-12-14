0090e7247d8c29b54efee8a15792857acd00399b
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2013-12-14 12:45:11 -0600

    removed unused code

--- liber-web/src/main/webapp/viewmodels/fields.js
+++ liber-web/src/main/webapp/viewmodels/fields.js
@@ -3,25 +3,7 @@ define( ['knockout', 'liber/fieldRepository'],
 		var model = function FieldViewModel() {
 	        var self = this;
 	        
-	        self.activeField = ko.observable();
 	        self.fields = ko.observableArray( [] );
-
-	        self.listingView = "listing";
-	        self.viewView = "view";
-	        self.view = ko.observable( self.listingView );
-	        self.isListingView = ko.computed(
-                function() {
-                    return self.view() == self.listingView;
-                }
-	        );
-	        self.isViewView = ko.computed(
-                function() {
-                	return self.view() == self.viewView;
-                }
-	        );
-	        self.goToListingView = function() {
-                self.view( self.listingView );
-	        };
 	        
 	        self.activate = function() {
 	        	fieldRepository.retrieveFields( self.fields );
