a9bb736ff0c325af7d62fdbdf335d4aaa426e072
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2014-03-09 20:06:02 -0500

    joined latest content version and content field values

--- liber-services-node/app.js
+++ liber-services-node/app.js
@@ -14,7 +14,7 @@ function initializeApp( config ) {
 
 function initializePersistence( config ) {
 	sequelize = new Sequelize( config.databaseName, config.databaseUserName, config.databasePassword );
-	Article = sequelize.define( 'article', 
+	Content = sequelize.define( 'content', 
 									{
 										id: Sequelize.INTEGER,
 										name: Sequelize.STRING
@@ -22,20 +22,64 @@ function initializePersistence( config ) {
 									{
 										tableName: 'article'
 									} );
+	ContentVersion = sequelize.define( 'contentVersion',
+										{
+											id: Sequelize.INTEGER
+										},
+										{
+											tableName: 'article_version',
+											timestamps: false
+										} );
+	ContentVersion.hasOne( Content, { as: 'latestVersion', foreignKey: 'latest_version' } );
+	Content.belongsTo( ContentVersion, { as: 'latestVersion', foreignKey: 'latest_version' } );
+	ContentFieldValue = sequelize.define( 'contentFieldValue',
+											{
+												id: Sequelize.INTEGER,
+												value: Sequelize.STRING
+											},
+											{
+												tableName: 'content_field_value',
+												timestamps: false
+											} );
+	ContentVersion.hasMany( ContentFieldValue, { foreignKey: 'article_version_id' } );
 }
 
 function initializeServer( config ) {
-	var retrieveArticles = function( request, response, next ) {
-		Article.findAll().success( function( articles ) { response.send( articles ); } );
+	var retrieveContents = function( request, response, next ) {
+		Content.findAll( { include: [{
+										model: ContentVersion, 
+										as: 'latestVersion', 
+										include: [{ model: ContentFieldValue }] }] } ).success( 
+			function( contents ) {
+				var restfulContents = contents.map(
+					function( content ) {
+						var newContent = {};
+						newContent.id = content.id;
+						newContent.name = content.name;
+						newContent.fields = content.latestVersion.contentFieldValues.map(
+							function( contentFieldValue ) {
+								var field = {};
+								//field.name = contentFieldValue.name;
+								//field.type = contentFieldValue.type;
+								field.value = contentFieldValue.value;
+								return field;
+							}
+						);
+						return newContent;
+					}
+				);
+				response.send( restfulContents );
+			}
+		);
 	};
-	var retrieveArticle = function( request, response, next ) {
+	var retrieveContent = function( request, response, next ) {
 		var id = request.params.id;
-		Article.find( id ).success( function( article ) { response.send( article ); } );
+		Content.find( id ).success( function( content ) { response.send( content ); } );
 	};
 
 	var server = restify.createServer();
-	server.get( '/articles', retrieveArticles );
-	server.get( '/articles/:id', retrieveArticle );
+	server.get( '/articles', retrieveContents );
+	server.get( '/articles/:id', retrieveContent );
 
 	server.listen( config.port, config.host, function() {
 		console.log( '%s listening at %s', server.name, server.url );
