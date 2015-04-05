bb198571ed5fb661830314a2f25b26c6bb9a8674
jessa <jessalyn.sparks@gmail.com>
2015-04-04 21:32:26 -0500

    Added filter by content type - https://trello.com/c/cUoUidCW

--- web/app/content/content-listing-controller.js
+++ web/app/content/content-listing-controller.js
@@ -3,8 +3,22 @@ angular.module( 'content-listing-controller', [] ).controller(
     [
         '$scope',
         'ContentService',
-        function( $scope, ContentService ) {
+        'ContentTypeService',
+        function( $scope, ContentService, ContentTypeService ) {
             $scope.content = ContentService.query();
+            ContentTypeService.query(
+                function( contentTypes ) {
+                    $scope.contentTypes = contentTypes;
+                }
+            );
+
+            $scope.filterByContentType = function(content) {
+                return !$scope.contentTypeFilter || $scope.contentTypeFilter._id === content.contentType
+            }
+
+            $scope.clearFilter = function() {
+                $scope.contentTypeFilter = null;
+            }
 	   }
     ]
 );
