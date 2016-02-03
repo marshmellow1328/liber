167ba79fe904e04b5440b0b075d587c83e249823
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2016-02-02 21:40:16 -0600

    runner script cleans up any leftover processes before trying to start them

--- runner.sh
+++ runner.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
+pkill grunt
+pkill node
+
 json-proxy -p 8080 -f "/api=http://localhost:8081" -f "/liber=http://localhost:8082" &
 cd /vagrant/services
 grunt &
 cd /vagrant/web
 grunt &
-
-#pkill grunt
-#pkill json-proxy
